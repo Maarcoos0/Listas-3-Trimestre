adb18d857cf26430724b7bd35cefb1b333ffa94b
Marcos0 <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-10-11 09:48:50 -0300

    //Primeiro exércicos
    Conclusão exercício 1

---
+++ Exercício1.js
@@ -0,0 +1,12 @@
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 4;
+
+const soma = nota1 + nota2 + nota3;
+const divisao = soma / 3
+
+if(divisao > 6){
+    console.log(Aprovado);
+} else {
+    console.log(Reprovado)
+}
