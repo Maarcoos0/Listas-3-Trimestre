df8dfefa35d6d6e888109f3865486b7cf124c2e4
Maarcoos0 <marcos.pauloferreirade.moraes@escola.pr.gov.br>
2024-10-20 23:28:05 -0300

    Mais Exercícios concluídos
    Conclusão de exercícios

---
+++ Exercício2.js
@@ -0,0 +1,15 @@
+let a = 3;
+let b = 4;
+let c = 5;
+
+if(a > b && a > c) {
+    console.log('${a}É o maior')
+} 
+
+if(b > a && b > c) {
+    console.log('${b}É o maior')
+}
+
+if(c > b && c > a) {
+    console.log('${c}É o maior')
+} 
